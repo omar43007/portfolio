da76fb9929f08b0003e936e70948cab6bb627b54
omar43007 <oali43007@gmail.com>
2026-05-06 03:50:18 +0300

    fix merge conflict

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import Logo from"../assets/visual-studio.svg"
 
 function Navbar() {
   return (
-    <div className="flex-shrink-0">
+    <div className="shrink-0">
       <div className="w-full h-8 bg-[#201a2c] flex justify-between items-center">
         <ul className="flex list-none gap-4 text-[12px] px-2 py-1 text-gray-400">
           <li><img src={Logo} alt="logo" className="w-5 h-5" /></li>
